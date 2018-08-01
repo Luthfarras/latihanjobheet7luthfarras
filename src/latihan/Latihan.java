f28d71ba5d8ec2924050e8dfaa6ae791518a9005
Lenovo400S <Lenovo400S@DESKTOP-FNDOPOS>
2018-08-01 09:20:48 +0700

    Initial Commit

---
+++ src/latihan/Latihan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Lenovo400S
+ */
+public class Latihan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String identitas = "luthfarras / xrpl5/ 19";
+        System.out.println("identitas : "+ identitas);
+        
+        // TODO code application logic here
+    }
+    
+}

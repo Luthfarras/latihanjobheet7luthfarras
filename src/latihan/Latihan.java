faf5899b0d98fd7932a5aa2e2a58db53f30c1030
Lenovo400S <Lenovo400S@DESKTOP-FNDOPOS>
2018-08-01 21:57:28 +0700

    Coba Nested IF

--- src/latihan/Latihan.java
+++ src/latihan/Latihan.java
@@ -62,6 +62,14 @@ public class Latihan {
              } else {
                  System.out.println("nilai a lebih dari 6");
              }
+             System.out.println("\nNested IF");
+             if (a < 7) {
+                 System.out.println("nilai a kurang dari 7");
+                 if (a < 2)
+                     System.out.println("nilai a kurang dari 2");
+                 if (a < 4)
+                     System.out.println("nilai a kurang dari 4");
+             }
         // TODO code application logic here4
     }
     

364f16b84beb778bfee7b37c02b8e05cdbbb2b85
Lenovo400S <Lenovo400S@DESKTOP-FNDOPOS>
2018-08-03 18:32:43 +0700

    Initial Commit

--- src/latihan/CobaPercabangan.java
+++ src/latihan/CobaPercabangan.java
@@ -5,8 +5,6 @@
  */
 package latihan;
 
-import java.util.Scanner;
-
 /**
  *
  * @author Lenovo400S
@@ -15,67 +13,6 @@ public class CobaPercabangan {
     public static void main(String[] args) {
         String identitas = "Luthfarras Tresanizega / XRPL5 / 19";
         System.out.println("identitas : "+ identitas);
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nilai a ? ");
-        int a = scanner.nextInt();
-        
-        System.out.println("\nIF.. 1");
-        if (a < 5) System.out.println("nilai a kurang dari 5");
-        
-        System.out.println("\nIF..2");
-        if (a == 5)
-             System.out.println("nilai a sama dengan 5");
-        
-        System.out.println("\nIF.. 3");
-        if (a > 5);
-             System.out.println("isi variabel a : "+ a);
-             System.out.println("nilai a lebih dari 5");
-        
-              System.out.println("\nIF..ELSE..");
-             if (a > 5) {
-                 System.out.println("isi variabel a : "+ a);
-                 System.out.println("nilai a kurang dari 5");
-             } else System.out.println("nilai a lebih dari atau sama dengan 5");
-             
-             System.out.println("\nIF..ELSE IF..ELSE.. 1");
-             if (a < 5) {
-                 System.out.println("nilai a kurang dari 5");
-             } else if (a == 5) {
-                 System.out.println("nilai a sama dengan 5");
-             } else
-                 System.out.println("nilai a lebih dari 5");
-             
-             System.out.println("\nIF..ELSE IF..ELSE.. 2");
-             if (a < 2) {
-                 System.out.println("nilai a kurang dari 2");
-             } else if (a < 4) {
-                 System.out.println("nilai a kurang dari 4");
-             } else if (a < 6) {
-                 System.out.println("nilai a kurang dari 6");
-             } else if (a == 6) {
-                 System.out.println("nilai a sama dengan 6");
-             } else {
-                 System.out.println("nilai a lebih dari 6");
-                 
-                 System.out.println("\nNested IF");
-             if (a < 7) {
-                 System.out.println("nilai a kurang dari 7");
-                 if (a < 2)
-                     System.out.println("nilai a kurang dari 2");
-                 if (a < 4)
-                     System.out.println("nilai a kurang dari1 4");
-                 
-                 System.out.println("\nSWITCH..CASE");
-             switch(a) {
-                 case 1: System.out.println("nilai a => 1"); break;
-                 case 2: System.out.println("nilai a => 2"); break;
-                 case 3:
-                 case 4: System.out.println("nilai a => 3 atau 4"); break;
-                 case 5: System.out.println("nilai a => 5"); break;
-                 default: System.out.println("nilai a bukan antara 1-5");
-             }
     }
-    }
-}
+    
 }

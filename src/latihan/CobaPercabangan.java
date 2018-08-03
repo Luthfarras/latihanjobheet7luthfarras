fd4613bac3e8b8ee0e0d1982adae11079fb48d64
Lenovo400S <Lenovo400S@DESKTOP-FNDOPOS>
2018-08-03 18:42:14 +0700

    Coba IF

--- src/latihan/CobaPercabangan.java
+++ src/latihan/CobaPercabangan.java
@@ -5,6 +5,8 @@
  */
 package latihan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Lenovo400S
@@ -13,6 +15,22 @@ public class CobaPercabangan {
     public static void main(String[] args) {
         String identitas = "Luthfarras Tresanizega / XRPL5 / 19";
         System.out.println("identitas : "+ identitas);
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. 1");
+        if (a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF..2");
+        if (a == 5)
+             System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if (a > 5);
+             System.out.println("isi variabel a : "+ a);
+             System.out.println("nilai a lebih dari 5");
     }
     
 }

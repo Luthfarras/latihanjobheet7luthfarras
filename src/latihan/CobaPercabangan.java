b2ae86e4d3a00a0cb7ef4287f372773bd75391a6
Lenovo400S <Lenovo400S@DESKTOP-FNDOPOS>
2018-08-03 13:19:32 +0700

    Coba Nested IF

--- src/latihan/CobaPercabangan.java
+++ src/latihan/CobaPercabangan.java
@@ -65,6 +65,16 @@ public class CobaPercabangan {
                      System.out.println("nilai a kurang dari 2");
                  if (a < 4)
                      System.out.println("nilai a kurang dari1 4");
+                 
+                 System.out.println("\nSWITCH..CASE");
+             switch(a) {
+                 case 1: System.out.println("nilai a => 1"); break;
+                 case 2: System.out.println("nilai a => 2"); break;
+                 case 3:
+                 case 4: System.out.println("nilai a => 3 atau 4"); break;
+                 case 5: System.out.println("nilai a => 5"); break;
+                 default: System.out.println("nilai a bukan antara 1-5");
+             }
     }
     }
 }

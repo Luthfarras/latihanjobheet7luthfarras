bdc34cf7fc9c2cb9f2bc1d1e7fd6115a4d12aeb4
Lenovo400S <Lenovo400S@DESKTOP-FNDOPOS>
2018-08-03 11:16:57 +0700

    Initial Commit

---
+++ src/latihan/SaranBahan2Resep.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Lenovo400S
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+        String identitas = "Luthfarras Tresanizega / XRPL5 / 19";
+        System.out.println("Identitas :" + identitas);
+        
+    }
+    
+}
